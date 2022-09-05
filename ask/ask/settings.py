14357c10ce6926e7fe5040a01d32101e632b52d5
sergpet93 <sergpet93@mail.ru>
2022-09-05 20:11:47 +0000

    commit django

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gd=&6t+p!t1aci#k6vigdz)$)!fu%tik5pwvlw=1z)w@p=t@y5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c hello.py hello:wsgi_application
+gunicorn -c hello.py hello:wsgi_application & 
+gunicorn -c /home/box/web/etc/qa.py ask.wsgi:application
