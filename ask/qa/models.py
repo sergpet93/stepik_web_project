53e1671821fe35920dbeda9ee77ba4e669d4b389
sergpet93 <sergpet_93@mail.ru>
2022-09-11 22:41:55 +0000

    commit web

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,23 +1,25 @@
-from django.contrib.auth.models.User
+from django.contrib.auth.models import User
 from django.db import models
 class Question(models.Model):
 	title = models.CharField(max_length = 255)
 	text = models.TextField()
 	added_at = models.DateTimeField(auto_now_add = True)
 	rating = models.IntegerField(default = 0)
-	author = models.CharField(max_length = 255)
-	likes = models.TextField()
-	class QuestionManager(models.Manader):
-		def new(self):
-			return self.order_by('added_at')
-		def popular(self):
-			return self.order_by('-rating')
+	author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+	likes = models.ManyToManyField(User, related_name='question_like_user')
+	objects = QuestionManager()
+
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('added_at')
+	def popular(self):
+		return self.order_by('-rating')
 
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(auto_now_add = True)
-	question = models.CharField(max_length = 255)
-	author = models.CharField(max_length = 255)
+	question = models.ForeignKey(Question, null=True, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
 
 
