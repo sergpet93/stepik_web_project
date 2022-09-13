66e17fe80a4696b80ae57dd6d8bb31e7dcdee43c
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-13 16:06:23 +0300

    Commit web

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database web;"
 mysql -uroot -e "grant all privileges on web.*to'box'@'localhost' with grant option;"
-~/web/ask/manage.py makemigrations
-~/web/ask/manage.py migrate
+~/web/ask/manage.py makemigrations qa
+~/web/ask/manage.py migrate qa
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 gunicorn -c hello.py hello:wsgi_application & 
