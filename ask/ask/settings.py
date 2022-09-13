73da3894482acc4bd7a48fc0d1770cc2065dfb77
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-13 16:11:38 +0300

    Commit web

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
        # 'ENGINE': 'django.db.backends.sqlite3',
        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	'ENGINE': 'django.db.backends.mysql',
-	'NAME': os.path.join(BASE_DIR, 'web')
+	'NAME': os.path.join(BASE_DIR, 'web'),
 	#'NAME': 'web',
 	'USER': 'box',
     }
