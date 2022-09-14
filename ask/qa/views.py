13ae0fc57b7e1603d56b497eaa8210cf8a9c5b3d
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-14 22:41:27 +0300

    commit web

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -7,26 +7,23 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 def main(request, *args, **kwargs):
-	question = Question.objects.order_by('-added_at')
-	page = paginate(request, question )
-	return render (request, 'main.html',
-                   {
-                       'questions': page.object_list,
-                       'page': page,
-                   })
+    question = Question.objects.order_by('-added_at')
+    page = paginate(request, question )
+    return render (request, 'main.html', {
+        'questions': page.object_list,
+        'page': page,
+    })
 def popular(request):
-	popul = Question.objects.popular()
-	page = paginate(request, popul)
-	return render(request, 'main.html',
-                  {
-                      'questions': page.object_list,
-                      'page': page,
-                  })
+    popul = Question.objects.popular()
+    page = paginate(request, popul)
+    return render(request, 'main.html', {
+        'questions': page.object_list,
+        'page': page,
+    })
 
 def question(request, pk):
-	qst = get_object_or_404(Question, ip = pk)
-	return render (request, 'question.html', 
-		{
-			'question': qst,
-			'answers': qst.answer_set.all(),
-		})
+    qst = get_object_or_404(Question, ip = pk)
+    return render (request, 'question.html', {
+        'question': qst,
+        'answers': qst.answer_set.all(),
+    })
