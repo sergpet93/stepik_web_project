61408d4222e0056522edc77bdc312eae191cb3d9
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-18 23:57:04 +0300

    commit box

--- init.sh
+++ init.sh
@@ -2,9 +2,10 @@ sudo /etc/init.d/mysql start
 #mysql -uroot -e "create database web;"
 #mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box';"
 #mysql -uroot -e "grant all privileges on web.*to'box'@'localhost' with grant option;"
-sudo mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8;"
-sudo mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'pass';"
-sudo mysql -uroot -e "GRANT ALL ON ask.* TO 'sa'@'localhost';"
+sudo mysql -u root -e "CREATE DATABASE stepik_course;"
+sudo mysql -u root -e "CREATE USER box@'%' IDENTIFIED BY 'box';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON stepic_course.* TO box@'%' WITH GRANT OPTION;"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
 ~/web/ask/manage.py makemigrations qa
 ~/web/ask/manage.py migrate qa
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
