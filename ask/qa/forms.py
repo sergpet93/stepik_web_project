ba60a8dfb034bc596449931c8b8412e97513d061
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-18 20:53:22 +0300

    commit web

---
+++ ask/qa/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.forms import ModelForm
+from .models import Question, Answer
+
+class AskForm(forms.Form):
+  title = forms.CharField(max_length=256)
+  text = forms.CharField(widget=Forms.Textarea)
+  
+  def clean_title(self):
+    title = self.cleaned_data['title']
+    if title.strip() == '':
+      raise forms.ValidationError('Не корректный ввод данных', code='validation_error')
+    return title
+  
+  def clean_text(self):
+    text = self.cleaned_data['text']
+    if text.strip() == '':
+      raise forms.ValidationError('Не корректный ввод данных', code='validation_error')
+    return text
+  
+  def save(self):
+    if self._user.is_anonymous():
+      self.cleaned_data['author'] = 1
+    else:
+      self.cleaned_data['author'] = self._user
+    question = Question(**self.cleaned_data)
+    question.save()
+    return question
+  
+class AnswerForm(forms.Form):
+  text = forms.CharField(widget=Forms.Textarea)
+  question = forms.IntegerField(widget=forms.HiddenInput)
+  
+  def clean_text(self):
+    text = self.cleaned_data['text']
+    if text.strip() == '':
+      raise forms.ValidationError('Не корректный ввод данных', code='validation_error')
+    return text
+  
+  def clean_question(self):
+    try:
+      question = int(self.cleaned_data['question'])
+    except ValueError:
+      raise forms.ValidationError('Invalid data', code='validation_error')
+    return question
+  
+  def save(self):
+    self.cleaned_data['question'] = get_object_or_404(Question, pk=self.cleaned_data['question'])
+    if self._user.is_anonymous():
+      self.cleaned_data['author'] = 1
+    else:
+      self.cleaned_data['author'] = self._user
+    answer = Answer(**self.cleaned_data)
+    answer.save()
+    return answer
+  
+
