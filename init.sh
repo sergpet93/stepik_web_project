31970eb3d7f7bbaf2b790f8542c4c9b09b3349bf
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-18 23:59:59 +0300

    commit web

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@ sudo /etc/init.d/mysql start
 #mysql -uroot -e "create database web;"
 #mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box';"
 #mysql -uroot -e "grant all privileges on web.*to'box'@'localhost' with grant option;"
-sudo mysql -u root -e "CREATE DATABASE stepik_course;"
+sudo mysql -u root -e "CREATE DATABASE web;"
 sudo mysql -u root -e "CREATE USER box@'%' IDENTIFIED BY 'box';"
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON stepic_course.* TO box@'%' WITH GRANT OPTION;"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON web.* TO box@'%' WITH GRANT OPTION;"
 sudo mysql -u root -e "FLUSH PRIVILEGES;"
 ~/web/ask/manage.py makemigrations qa
 ~/web/ask/manage.py migrate qa
