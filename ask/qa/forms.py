a844baf80b7355ebdeb5a985370ef179d2318d4a
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-18 23:06:10 +0300

    commit web

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -21,11 +21,12 @@ class AskForm(forms.Form):
     return text
   
   def save(self):
-    if self._user.is_anonymous():
-      self.cleaned_data['author_id'] = 1
-    else:
-      self.cleaned_data['author'] = self._user
+    #if self._user.is_anonymous():
+     # self.cleaned_data['author_id'] = 1
+    #else:
+     # self.cleaned_data['author'] = self._user
     question = Question(**self.cleaned_data)
+    question.author_id = 1
     question.save()
     return question
   
@@ -48,11 +49,12 @@ class AnswerForm(forms.Form):
   
   def save(self):
     self.cleaned_data['question'] = get_object_or_404(Question, pk=self.cleaned_data['question'])
-    if self._user.is_anonymous():
-      self.cleaned_data['author_id'] = 1
-    else:
-      self.cleaned_data['author'] = self._user
+    #if self._user.is_anonymous():
+     # self.cleaned_data['author_id'] = 1
+    #else:
+     # self.cleaned_data['author'] = self._user
     answer = Answer(**self.cleaned_data)
+    answer.author_id = 1
     answer.save()
     return answer
   
