16a52f2855e80367e600cd6106eb76981ba0c8f0
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-18 21:03:03 +0300

    commit web

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from qa.itels import paginate
 from qa.models import Question, Answer
+from .forms import AskForm, AnswerForm
+from django.views.decorators.http import require_GET
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
@@ -23,7 +25,31 @@ def popular(request):
 
 def question(request, id):
     qst = get_object_or_404(Question, id = id)
+    form = AnswerForm(initial = {'question': question_id})
     return render (request, 'question.html', {
         'question': qst,
         'answers': qst.answer_set.all(),
-    })
+        'form': form,
+    }) 
+
+def ask(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            question = form.save()
+            url = question.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AskForm()
+    return render(request, 'ask.html', {'form': form})
+
+def answer(request):
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            answer = form.save()
+            url = answer.get_url()
+            return HttpResponseRedirect(url)
+    return HttpResponseRedirect('/')
