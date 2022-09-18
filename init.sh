a9cb5643bd17bba3c5a23e3abec69ca6d77e4562
sergpet93 <97629204+sergpet93@users.noreply.github.com>
2022-09-19 00:14:37 +0300

    commit web

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 sudo /etc/init.d/mysql start
-#mysql -uroot -e "create database web;"
+mysql -uroot -e "create database web;"
 #mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box';"
-#mysql -uroot -e "grant all privileges on web.*to'box'@'localhost' with grant option;"
-sudo mysql -u root -e "CREATE DATABASE web;"
-sudo mysql -u root -e "CREATE USER box@'%' IDENTIFIED BY 'box';"
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON web.* TO box@'%' WITH GRANT OPTION;"
-sudo mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "grant all privileges on web.*to'box'@'localhost' with grant option;"
+#sudo mysql -u root -e "CREATE DATABASE web;"
+#sudo mysql -u root -e "CREATE USER box@'%' IDENTIFIED BY 'box';"
+#sudo mysql -u root -e "GRANT ALL PRIVILEGES ON web.* TO box@'%' WITH GRANT OPTION;"
+#sudo mysql -u root -e "FLUSH PRIVILEGES;"
 ~/web/ask/manage.py makemigrations qa
 ~/web/ask/manage.py migrate qa
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
